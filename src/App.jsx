c0e486288461190b10df5f3c1efa2882f34c0fde
Robert Enache <robert.enache00@gmail.com>
2022-11-21 11:53:47 +0200

    delete cards with index

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,33 @@ function App() {
     const text = document.getElementById("toDoInput").value;
     setCards([...cards,text]);
   }
+  function deleteCard(index){
+    if(window.confirm(`Are you sure you want to delete the card with index ${index+1}?`)){
+      const copy = cards.filter((_, i) => i !== index);
+      setCards(copy);
+    }
+    
+  }
+  useEffect(()=>{
+    console.log(cards)
+  },[cards]);
   return (
     <>
     <div className='form'>
       <h1>TO DO</h1>
       <input id="toDoInput"></input>
-      <button onClick={onAddBtnClick}>GO</button>
+      <button type="button" onClick={onAddBtnClick}>GO</button>
     </div>
     <div className='cards-holder'>
-      {cards.map((item,i) => (<Card text={item}/>))}
+      {cards.map((item,i) => 
+      (<Card key={i}  
+        onClick={(e) =>{
+        deleteCard(i);
+      }} 
+        number={i+1} 
+        text={item} 
+        />))
+        }
     </div>
     </>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,12 @@ import React from "react";
 
 const Card = (props) => {
     return (
-        <div>
-            <p>{props.text}</p>
+        <div className='card'>
+            <div className='card-header'>
+                <p className='card-number'>{props.number}</p>
+                <button type="button" onClick={props.onClick} className='delete-card-button'>X</button>
+            </div>
+            <p className='card-text'>{props.text}</p>
         </div>
     );
 };
